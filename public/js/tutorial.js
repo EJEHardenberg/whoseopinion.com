6f83df9b67e602ebcb6aa84aa84614f0a1aa2311
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-08-27 08:35:08 -0400

    Beginning to Plan a tutorial.
    Going to use JS and tooltipster to create helpful tooltips that will be
    interacted with by a user and result in their education.

---
+++ public/js/tutorial.js
@@ -0,0 +1,12 @@
+jQuery( document ).ready(function( $ ) {
+	console.info('tutorial.js loaded')
+	/* Check for cookie that says not to show the tutorial */
+
+	/* Display the first area to point to and click for the tutorial */
+
+	/* Continue with adding events and states and things to teach the user */
+
+	/* At any point they stop, remember their choice and do not bother them again */
+
+	
+})
