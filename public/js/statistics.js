d3b700eb79dec9181a018a15f101cfc6dc917ff4
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-08-27 19:24:36 -0400

    Map now highlights on hovering the vote.

--- public/js/statistics.js
+++ public/js/statistics.js
@@ -1,7 +1,40 @@
 $(document).ready(function() {
     $('#map').usmap({
     	stateSpecificStyles: window.dummyMapInfo,
-  		stateHoverStyles: {fill: 'white'}
+  		stateHoverStyles: {fill: 'white'},
+  		showLabels: true
     });
+    var stateMapping = {
+        "vote-Outraged" : ['MD'],
+        "vote-Disagree" : ["VA"],
+        "vote-Neutral" : ["GA"],
+        "vote-Agree" : ["MO"],
+        "vote-Support" : ["CA"]
+    }
+
+    function triggerState(classes,mapevent){
+        var datum = stateMapping
+        for( idx in classes){
+            if( classes[idx] in datum ){
+                var k =classes[idx]
+                for (var i = datum[k].length - 1; i >= 0; i--) {
+                    var state = datum[k][i]
+                    $('#map').usmap('trigger', state, mapevent, null)         
+                };
+            }
+        }
+    }
+    /* Bind events to the map in order to trigger flashing or highlighting the
+     * states that should be highlighted by the statistic
+     */
+     $('#statistics li.vote').on("mouseover",function(){
+     	var classes = this.className.split(" ") 
+        triggerState(classes, "mouseover")     	
+     })
+     $('#statistics li.vote').on("mouseout",function(){
+     	var classes = this.className.split(" ") 
+        triggerState(classes, "mouseout")      
+     })
+     
 });
 
