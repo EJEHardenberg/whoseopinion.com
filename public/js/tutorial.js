7b7bce5aa9088966efddaf483be4827860fed0a6
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-09-01 21:33:28 -0400

    Added tooltip popup when clicking the help button.

--- public/js/tutorial.js
+++ public/js/tutorial.js
@@ -8,10 +8,46 @@ jQuery( document ).ready(function( $ ) {
 
 	/* At any point they stop, remember their choice and do not bother them again */
 
-	function categoryHelp(){
-		$('[name=category-header]').tooltipster({
-			content: 'Select a category below to display statements to vote on'
-		})
+	var show = false
+	function showHideTip(tip){
+		/* Uses global show var */
+		if(show) tip.tooltipster('show')
+		else tip.tooltipster('hide')
 	}
+	
+	$('[name=vote-help]').tooltipster()
+	$('[name=vote-help]').click(function(){ 
+		show = !show 
+		categoryHelp()
+		otherOppHelp()
+		voiceYours()
+		$('.tooltipstered').tooltipster( show ? 'enable' : 'disable')
+	})
+
+	function categoryHelp(){
+		var tip = $('[name=category-header]')
+		tip.tooltipster({
+			content: 'Select a category below to display statements to vote on',
+		})
+		showHideTip(tip)
+	}
+
+	function otherOppHelp(){
+		var tip = $('[name=loadstats]')
+		tip.tooltipster({
+			content: 'Show other user\'s votes, see how regions think, and view more information on the subject'
+		})
+		showHideTip($(tip[2]))
+	}
+
+	function voiceYours(){
+		var tip = $('[name=showscale]')
+		tip.tooltipster({
+			content: 'Click to reveal the voting scale'
+		})
+		showHideTip($(tip[4]))
+	}
+
+	
 
 })
