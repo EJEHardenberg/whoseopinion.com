b2806a72dfe965ad9bfa98f947ef9fe33e1bc9b2
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-09-09 09:42:36 -0400

    Started writing method tests.

--- api/questions/tests.py
+++ api/questions/tests.py
@@ -1,3 +1,36 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+
+from questions.models import Question,Opinion,Category
 
 # Create your tests here.
+class QuestionMethodTests(TestCase):
+	
+	def test_is_recent_with_future_question(self):
+		"""
+		was is_recent() should return False for questions whose
+		pub_date is in the future
+		"""
+		time = timezone.now() + datetime.timedelta(days=30)
+		future_question = Question(pub_date=time)
+		self.assertEqual(future_question.is_recent(),False)
+	
+	def test_is_recent_with_old_questions(self):
+		"""
+		is_recent() should return False for questions 
+		whose pub_date is older than 1 day
+		"""
+		time = timezone.now() - datetime.timedelta(days=30)
+		old_question = Question(pub_date=time)
+		self.assertEqual(old_question.is_recent(), False)
+	
+	def test_is_recent_with_recent_question(self):
+		"""
+		is_recent() should return True for
+		questions whose pub_date is within the last day
+		"""
+		time = timezone.now() - datetime.timedelta(hours=1)
+		recent_question = Question(pub_date=time)
+		self.assertEqual(recent_question.is_recent(), True)
