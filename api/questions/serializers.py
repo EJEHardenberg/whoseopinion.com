e4c04c9a248e7a1e0187ee198216ebc4771d70ed
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-09-09 09:37:33 -0400

    Removed Unneccesary code.
    Following along in tutorial, removed restore objet because it seems that
    ModelSerializer takes care of all that for us.

--- api/questions/serializers.py
+++ api/questions/serializers.py
@@ -4,31 +4,14 @@ from questions.models import Category, Question, Opinion
 class CategorySerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Category
-		fields = ('cat_name')
+		fields = ('cat_name',)
 
 class QuestionSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Question
-		fields = ('statement')
+		fields = ('statement','category','pub_date')
 
 class OpinionSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Opinion
 		fields = ('question','vote')
-
-	def restore_object(self, attrs, instance=None):
-		"""
-		Create or update a new opinion instance, given a dictionary
-		of deserialized field values.
-
-		Note that if we don't define this method, then deserializing
-		data will simply return a dictionary of items.
-		"""
-		if instance:
-			# Update existing instance
-			instance.title = attrs.get('question', instance.question)
-			instance.vote = attrs.get('vote', instance.vote)
-			return instance
-
-		# Create new instance
-		return Opinion(**attrs)
