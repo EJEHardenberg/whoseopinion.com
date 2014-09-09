f111253e6dee7ecf03f5ef9e71cfce9301bbd584
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-09-09 08:45:59 -0400

    Serialization for Opinion working.

--- api/questions/serializers.py
+++ api/questions/serializers.py
@@ -15,3 +15,20 @@ class OpinionSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Opinion
 		fields = ('question','vote')
+
+	def restore_object(self, attrs, instance=None):
+		"""
+		Create or update a new opinion instance, given a dictionary
+		of deserialized field values.
+
+		Note that if we don't define this method, then deserializing
+		data will simply return a dictionary of items.
+		"""
+		if instance:
+			# Update existing instance
+			instance.title = attrs.get('question', instance.question)
+			instance.vote = attrs.get('vote', instance.vote)
+			return instance
+
+		# Create new instance
+		return Opinion(**attrs)
