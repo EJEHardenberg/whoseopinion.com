c92e5bf8d44ad957c8003522d74b75351f6a4c20
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-09-17 08:25:29 -0400

    Integrating Javascript with Django API

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,59 @@
 jQuery( document ).ready(function( $ ) {
 	console.info("Index.js loaded")	
+
+	var loadQuestionsEvent = "load_questions"
+	var popularCategoryFlag = -1
+
+	/* First, load the categories */
+	function catHandler(json){
+		/* Check that it's an array first */
+		if(!(json instanceof Array)){
+			alert("Could not load categories.")
+			return;
+		}
+
+		var ul = $('#category-list')
+		for (var i = json.length - 1; i >= 0; i--) {
+			var li = document.createElement("li")
+			var anchor = document.createElement("a")
+			$(anchor).text(json[i].cat_name)
+			$(anchor).attr('href', '/#category-list:' + json[i].id)
+			$(anchor).attr('rel', json[i].id)
+			$(li).append(anchor)
+			$(li).hide()
+			  ul.append(li)
+			$(li).fadeIn()
+		};
+		/* Fire the event handler to load popular questions */
+		$.event.trigger({
+			type: loadQuestionsEvent,
+			category: popularCategoryFlag
+		})
+	}
+	$.getJSON(window.apiuri + "categories/", catHandler)
+
+	function displayQuestions(json){
+		console.log('displayQuestions')
+
+	}
+
+	function loadQuestions(categoryId){
+		console.log('loadQuestions')
+		var categoryId = parseInt(categoryId)
+		if(isNaN(categoryId)){
+			console.debug("Invalid category id being passed to function")
+			return
+		}
+		var url = category/ + categoryId + '/'
+		$.getJSON(url, displayQuestions)
+	}
+	$('#category-list').on('click', 'a', function(evt){
+		var id = $(this).attr('href').split(':')[1]
+		$.event.trigger({
+			type: loadQuestionsEvent,
+			category: id
+		})
+	})
+
+	$(document).on(loadQuestionsEvent, loadQuestions)
 })
